0bf350bb8ede778c980d8c93a76d36ed9b05dc57
filbert campos <filbertcampos@gmail.com>
2025-01-29 17:37:07 -0300

    adicionando lista

--- app.js
+++ app.js
@@ -2,22 +2,24 @@
 let nomes = [];
 let sorteado;
 
-
 function adicionarAmigo() {
-    let nomeInput = document.querySelector('input').value
+    let nomeInput = document.querySelector('input').value;
+    let lista = document.querySelector('ul').innerHTML;
     if(nomeInput == ''){
         alert('Voce deve digitar um nome');
-    }else{
+    } else {
         nomes.push(nomeInput);
         document.querySelector('input').value = '';
-        nomeInput.value = '';
-        console.log(nomes);
-        
+        exibirTextoNaTela('ul', `nomes cadastrados: ${nomes.join(', ')}`);
     }
-    
-
 }
+
 function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirListaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
@@ -26,10 +28,8 @@ function sortearAmigo() {
     let quantidadeDeAmigos = nomes.length;
     if(quantidadeDeAmigos == 0){
         alert('Voce deve adicionar pelo menos um amigo');
-    }else{
+    } else {
         sorteado = nomes[parseInt(Math.random() * quantidadeDeAmigos)];
-        exibirTextoNaTela('h2', `O amigo sorteado é ${sorteado}`);
+        exibirTextoNaTela('ul', `O amigo sorteado é ${sorteado}`);
     }
-    
 }
-
